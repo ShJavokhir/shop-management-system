64c8c1b1eb0860082f7baaf22673f74b30692a24
Javokhir Shomuratov <javohirshomuradov@gmail.com>
2021-08-28 15:56:57 +0500

    Initial commit

---
+++ lib/app/widgets/left_menu.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class LeftMenu extends StatelessWidget {
+  const LeftMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
---
+++ lib/app/widgets/left_menu_item.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class LeftMenuItem extends StatelessWidget {
+  const LeftMenuItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
